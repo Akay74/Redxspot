d9a09114c1841232b96daa5c7f83a15236449ab0
Akay74 <akaudeh@gmail.com>
2025-04-08 08:49:58 +0100

    ⚡️ set material app theme, add Home screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      home: HomeScreen(),
     );
   }
 }
