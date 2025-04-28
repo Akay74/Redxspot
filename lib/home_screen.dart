89bfa63473ebf4e2789d042df7d81d62281fa885
Akay74 <akaudeh@gmail.com>
2025-04-28 12:23:19 +0100

    ✨ create most visited section

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -256,6 +256,73 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           )
         ),
+        // Most Visited Title
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('Most Visited',
+                style: TextStyle(
+                  fontSize: 21,
+                  fontWeight: FontWeight.w500,
+                )),
+              const Text('See all',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ))
+            ],
+          ),
+        ),
+        // Most Visited cards
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: SizedBox(
+            height: 170,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,itemCount: 3,
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              itemBuilder: (context, index) {
+              final placeData = [
+                {
+                  'image': 'assets/images/gustavo.png',
+                  'title': 'Gustavo by cubana',
+                  'location': 'GRA + 3 others',
+                  'subtitle': 'Dance club/Lounge',
+                  'rating': '★★★★★',
+                },
+                {
+                  'image': 'assets/images/gustavo.png',
+                  'title': 'Cynthia Garden',
+                  'location': 'Trans-Ekulu',
+                  'subtitle': 'Hotel/Gym',
+                  'rating': '★★★★★',
+                },
+                {
+                  'image': 'assets/images/gustavo.png',
+                  'title': 'Cynthia Garden',
+                  'location': 'Independence Layout',
+                  'subtitle': 'Hotel/Gym',
+                  'rating': '★★★★★',
+                },
+              ];
+              
+              return Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: PlaceCard(
+                  image: placeData[index]['image']!,
+                  title: placeData[index]['title']!,
+                  location: placeData[index]['location']!,
+                  subtitle: placeData[index]['subtitle']!,
+                  rating: placeData[index]['rating']!,
+                ),
+              );
+            },
+            ),
+          )
+        ),
       ]),
     );
   }
