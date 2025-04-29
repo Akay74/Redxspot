8273c720b2afd889f7f340cb340546b184206cc2
Akay74 <akaudeh@gmail.com>
2025-04-29 04:08:28 +0100

    🌱 create locations screen

---
+++ lib/locations.dart
@@ -0,0 +1,123 @@
+// hot_alerts_screen.dart
+import 'package:flutter/material.dart';
+import './widgets/place_card.dart';
+import './widgets/app_bar.dart';
+
+class LocationsScreen extends StatelessWidget {
+  const LocationsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: _buildLocationsContent(),
+    );
+  }
+
+  Widget _buildLocationsContent() {
+    return SingleChildScrollView(
+      child: Column(children: [
+        // hero image
+        SizedBox(
+          child: Image.asset('assets/images/locations_hero.png'),
+        ),
+        
+        // Restaurants section
+        _buildSection('Restaurants'),
+        _buildPlaceCards(),
+        
+        // Bars section
+        _buildSection('Bars'),
+        _buildPlaceCards(),
+
+        // Clubs section
+        _buildSection('Clubs'),
+        _buildPlaceCards(),
+
+        // Gym/Sports section
+        _buildSection('Gym/Sports'),
+        _buildPlaceCards(),
+
+        // Hotels section
+        _buildSection('Hotels'),
+        _buildPlaceCards(),
+
+        // Malls section
+        _buildSection('Malls'),
+        _buildPlaceCards(),
+      ]),
+    );
+  }
+
+  Widget _buildSection(String title) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title,
+              style: const TextStyle(
+                fontSize: 21,
+                fontWeight: FontWeight.w500,
+              )),
+          const Text('See all',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ))
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPlaceCards() {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: SizedBox(
+        height: 170,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 3,
+          padding: const EdgeInsets.symmetric(vertical: 4),
+          itemBuilder: (context, index) {
+            final placeData = [
+              {
+                'image': 'assets/images/gustavo.png',
+                'title': 'Gustavo by cubana',
+                'location': 'GRA + 3 others',
+                'subtitle': 'Dance club/Lounge',
+                'rating': '★★★★★',
+              },
+              {
+                'image': 'assets/images/gym.png',
+                'title': 'Cynthia Garden',
+                'location': 'Trans-Ekulu',
+                'subtitle': 'Hotel/Gym',
+                'rating': '★★★★★',
+              },
+              {
+                'image': 'assets/images/extreme_lounge.png',
+                'title': 'Extreme Lounge',
+                'location': 'Independence Layout',
+                'subtitle': 'Hotel/Gym',
+                'rating': '★★★★★',
+              },
+            ];
+            
+            return Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: PlaceCard(
+                image: placeData[index]['image']!,
+                title: placeData[index]['title']!,
+                location: placeData[index]['location']!,
+                subtitle: placeData[index]['subtitle']!,
+                rating: placeData[index]['rating']!,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
