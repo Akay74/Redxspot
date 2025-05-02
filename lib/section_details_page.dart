32aa445ef99292fe43307685337f0843709c10df
Akay74 <akaudeh@gmail.com>
2025-05-02 23:52:16 +0100

    ➕ import the section header and grid items widget

--- lib/section_details_page.dart
+++ lib/section_details_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './widgets/sections_app_bar.dart';
+import '../widgets/sections_app_bar.dart';
+import '../widgets/sections_header.dart';
+import '../widgets/place_card.dart';
 
 class SectionDetailsPage extends StatelessWidget {
   final String title;
